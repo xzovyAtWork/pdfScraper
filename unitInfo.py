f93117b637b46ee91deabc10db6b0c2968a69fe0
xzovyAtWork <jsteinca@jci.com>
2024-06-22 14:42:47 -0600

    wrap code in a function

--- unitInfo.py
+++ unitInfo.py
@@ -1,13 +1,17 @@
 from pypdf import PdfReader
 
-reader = PdfReader('3936-60L FT.pdf')
 
-unitNumber = reader.pages[0]
-signOffPage = reader.pages[11]
-unitInfoPage = unitNumber.extract_text().split(' ')
-unitInfo = unitInfoPage[37] + '-' + unitInfoPage[39] # + ' ' + unitInfoPage[40]
-technicianInfo = signOffPage.extract_text().split('SIGNATURE  DATE')
-signedOffBy = technicianInfo[1]
+def scrape_PDF(*fileName):   
+    reader = PdfReader('3936-60L FT.pdf')
+    # reader = PdfReader(fileName)
 
-print(unitInfo)
-print(signedOffBy)
+    unitNumber = reader.pages[0]
+    signOffPage = reader.pages[11]
+    unitInfoPage = unitNumber.extract_text().split(' ')
+    unitInfo = unitInfoPage[37] + '-' + unitInfoPage[39] # + ' ' + unitInfoPage[40]
+    technicianInfo = signOffPage.extract_text().split('SIGNATURE  DATE')
+    signedOffBy = technicianInfo[1]
+
+    print(unitInfo)
+    print(signedOffBy)
+    return unitInfo, signedOffBy
